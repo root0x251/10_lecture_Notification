ae5fbf0c9b3c201f5cf031680496f9d83fc73512
Slava <rtootik@gmail.com>
2017-02-28 19:46:20 +0300

    Initial Commit

---
+++ 10_lecture-Notification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  10_lecture-Notification
+//
+//  Created by Slava on 28.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 10_lecture-Notification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  10_lecture-Notification
+//
+//  Created by Slava on 28.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
